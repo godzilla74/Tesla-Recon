5754bdc040a1cfae210c528a71c8313b02e3f56c
root <root@localhost.localdomain>
2019-09-08 17:35:04 -0400

    Added Juicy

--- Recon/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-read -p "Enter The Site > " site 
-curl https://crt.sh/?q=%25.$site -o $site.txt &>/dev/null 
-echo "Downloading Page"
-sleep 15
-echo "Grabbing Subdomains"
-sleep 3
-bash -c "grep -o '[A-Za-z0-9.\_-]*\.*$site' $site.txt | sort -u"
--- Recon/status.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-bash -c "for lines in $(cat hosts);do curl -s -o /dev/null -I -w "%{http_code}" '$lines\n';done"
